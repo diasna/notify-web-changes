b8458feb486691f7b10e3df96cb2adf639cbffe1
diasna <dias.arifin@gmail.com>
2022-05-08 17:21:48 +0700

    multiple watch list

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -20,14 +21,14 @@ type TelegramConfiguration struct {
 	Token  string `json:"token"`
 	ChatId string `json:"chat_id"`
 }
-type MainConfiguration struct {
+type Watch struct {
 	Url      string `json:"url"`
 	Selector string `json:"selector"`
-	Interval int    `json:"interval_in_minutes"`
 }
 type Configuration struct {
-	Main     MainConfiguration     `json:"main"`
-	Telegram TelegramConfiguration `json:"telegram"`
+	Telegram  TelegramConfiguration `json:"telegram"`
+	Interval  int                   `json:"interval_in_minutes"`
+	WatchList []Watch               `json:"watch_list"`
 }
 
 func loadConfiguration(path string) Configuration {
@@ -62,14 +63,35 @@ func sendTelegramMessage(token string, chatId string, message string) bool {
 	}
 	if res != nil {
 		defer res.Body.Close()
-		body, err := ioutil.ReadAll(res.Body)
+		/*body, err := */ ioutil.ReadAll(res.Body)
 		if err != nil {
 			log.Printf("cannot send telegram message, Status: %s", res.Status)
 		}
-		log.Printf("Send notification response: %s\n", string(body))
+		// log.Printf("Send notification response: %s\n", string(body))
 		return true
 	}
 	return false
+}
+
+func extractNodes(url string, selector string) ([]string, error) {
+	doc, err := htmlquery.LoadURL(url)
+	if err != nil {
+		log.Printf("cannot load url: %s", err.Error())
+		return nil, errors.New("cannot load url: %s")
+	}
+	watch, err := htmlquery.QueryAll(doc, selector)
+	if err != nil || watch == nil {
+		log.Printf("cannot parse selector: %s", err.Error())
+		return nil, errors.New("cannot parse selector: %s")
+	}
+
+	var extracted []string
+	for _, n := range watch {
+		if n != nil {
+			extracted = append(extracted, fmt.Sprintf("%s(%s)\n", htmlquery.InnerText(n), htmlquery.SelectAttr(n, "class")))
+		}
+	}
+	return extracted, nil
 }
 
 func main() {
@@ -84,47 +106,33 @@ func main() {
 
 	sendTelegramMessage(configuration.Telegram.Token, configuration.Telegram.ChatId, "service notify-web-changes started")
 
-	var valueToWatch [10]string
+	var valueToWatch [10][10]string
 	isNotificationSent := true
+
 	for {
-		time.Sleep(time.Duration(configuration.Main.Interval) * time.Minute)
-		doc, err := htmlquery.LoadURL(configuration.Main.Url)
-		if err != nil {
-			log.Printf("cannot load url: %s", err.Error())
-			continue
-		}
-		watch, err := htmlquery.QueryAll(doc, configuration.Main.Selector)
-		if err != nil {
-			log.Printf("cannot parse selector: %s", err.Error())
-			continue
-		}
+		time.Sleep(time.Duration(configuration.Interval) * time.Minute)
 		var notificationMessage string
 		var valueChanged bool
-
-		if watch != nil {
-			for i, n := range watch {
-				if n != nil {
-					extracted := fmt.Sprintf("%s(%s)\n", htmlquery.InnerText(n), htmlquery.SelectAttr(n, "class"))
-
-					if valueToWatch[i] != extracted {
-						notificationMessage += extracted
-						valueChanged = true
-						log.Printf("Value has change: %s\n", extracted)
-					}
-
-					valueToWatch[i] = extracted
+		for i, n := range configuration.WatchList {
+			extracted, err := extractNodes(n.Url, n.Selector)
+			if err != nil {
+				sendTelegramMessage(configuration.Telegram.Token, configuration.Telegram.ChatId, "cannot parse")
+				continue
+			}
+			for j, m := range extracted {
+				if valueToWatch[i][j] != m {
+					notificationMessage += m
+					valueChanged = true
+					log.Printf("%s => %s\n", valueToWatch[i][j], m)
 				}
+				valueToWatch[i][j] = m
 			}
-			if valueChanged || !isNotificationSent {
-				isNotificationSent = sendTelegramMessage(configuration.Telegram.Token, configuration.Telegram.ChatId, notificationMessage)
-			} else {
-				log.Println("No change")
-			}
-			valueChanged = false
-		} else {
-			log.Printf("watch is nill")
-			sendTelegramMessage(configuration.Telegram.Token, configuration.Telegram.ChatId, "watch is nill")
 		}
-
+		if valueChanged || !isNotificationSent {
+			isNotificationSent = sendTelegramMessage(configuration.Telegram.Token, configuration.Telegram.ChatId, notificationMessage)
+		} else {
+			log.Println("No change")
+		}
+		valueChanged = false
 	}
 }
